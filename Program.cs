a7323c333edfe1773d8945b4988ba56489e79616
Samuel Janetzki <samuel.janetzki@blackbook.ai>
2019-10-20 23:36:05 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using Microsoft.Office.Interop.Outlook;
+
+namespace SendHangoverEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // init args, can get from config, args or hard code.
+            String filename = "C:\\Users\\spamish\\Documents\\Personal.xlsb";
+            MailAddress recipient = new MailAddress("samueljanetzki@gmail.com");
+            String subject = "Test email";
+
+            // error checking here
+            if (!File.Exists(filename))
+            {
+                throw new FileNotFoundException();
+            }
+
+            // start application and build new email
+            Application app = new Application();
+            MailItem mail = (MailItem) app.CreateItem(OlItemType.olMailItem);
+            mail.Attachments.Add(filename);
+            mail.To = recipient.Address;
+            mail.Subject = subject;
+
+            // Send email, then send and receive all new mail.
+            mail.Send();
+            app.Session.SendAndReceive(false);
+            app.Quit();
+        }
+    }
+}
